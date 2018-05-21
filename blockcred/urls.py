69e78017be5a3ae94441b87da302f0710ab79fc2
minafarouk <minafarouk18@gmail.com>
2018-05-21 17:03:54 -0200

    removing qr_code from blockcred/urls.py

--- blockcred/urls.py
+++ blockcred/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import include, url
-from qr_code import urls as qr_code_urls
+
 
 urlpatterns = [
     path('creds/', include('creds.urls')),
