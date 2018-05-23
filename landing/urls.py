d8c0741da539b3a120b6552951b6af6102b2d3e3
minafarouk <minafarouk18@gmail.com>
2018-05-23 19:50:00 -0200

    contact function

--- landing/urls.py
+++ landing/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('', views.home, name='home'),
     path('faq/', views.faq, name='faq'),
+    path('contact/', views.contact, name='contact'),
     path('creds/', include('creds.urls')),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django_hosts.resolvers import reverse
+from .forms import *
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 
 def home(request):
     return render(request, 'landing/index.html')
 
+
 def faq(request):
     faq_url = reverse('faq', host='landing')
     return render(request, 'landing/faq.html',{'faq_url': faq_url})
+
+
+def contact(request):
+    form = ContactForm(request.POST)
+
+    if request.method == 'POST' and form.is_valid():
+        sender = settings.EMAIL_HOST_USER
+        to = settings.EMAIL_HOST_USER
+        send_mail(
+            form.cleaned_data['contact_name'] + ">>" + form.cleaned_data['contact_email'],  #subject
+            form.cleaned_data['content'],       #message
+            sender, [to])
+        returnedJSON = {}
+        returnedJSON['message'] = 'Your message sent successfully'
+        return JsonResponse(returnedJSON)
+
+    else:
+
+        return JsonResponse(form.errors.as_json(), safe=False,  status=400)
+
+
